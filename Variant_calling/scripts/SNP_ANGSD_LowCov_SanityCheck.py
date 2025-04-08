36e582a7db23a37cda5e1cf294c25d3d05789671
JolijnErven <jolijn_erven@hotmail.com>
2025-04-08 16:42:56 +0200

    Create SNP_ANGSD_LowCov_SanityCheck.py

---
+++ Variant_calling/scripts/SNP_ANGSD_LowCov_SanityCheck.py
@@ -0,0 +1,4 @@
+For pseudohaploid .haplo.gz files ; script is for python2.7
+Script created by Victoria E Mullin 2022
+
+Script can be found here: https://github.com/mullinv/general_scripts/blob/main/SNP_ANGSD_LowCov_SanityCheck.py
